d4de987ce5bd57b6fae34bf3b6a8047a756655e1
GCorbel <guirec.corbel@gmail.com>
2014-02-21 22:02:11 +0000

    change for udp

--- server.py
+++ server.py
@@ -3,17 +3,41 @@ import RPIO
 import socket
 import json
 import sys
+import threading
 
 servo = PWM.Servo()
 
-def socket_callback(socket, val):
-    received_text = val.decode()
-    for key, value in json.loads(received_text).items():
-        print("%d=%d" % (int(key), int(value)))
-        servo.set_servo(int(key), int(value))
-    socket.send(b"ok")
+watchdog = 10
 
-RPIO.add_tcp_callback(8081, socket_callback)
+def check ():
+    global watchdog
+    threading.Timer(1, check).start ()
+    watchdog -= 1
+    print (watchdog)
+    if watchdog == 0:
+        servo.set_servo(25, 1090)
+        watchdog = 10
+
+
+port = 8081
+
+check()
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(("", port))
+print ("waiting on port: ", port)
+while 1:
+
+    recv_data, addr = s.recvfrom(1024)
+
+    received_text = recv_data.decode()
+
+    for key, value in json.loads(received_text).items():
+        if int(key) == 0 :
+            watchdog = 10
+
+        else:
+            print("%d=%d" % (int(key),int(value)))
+            servo.set_servo(int(key), int(value))
 
 print("ready")
 servo.set_servo(18, 1500)
